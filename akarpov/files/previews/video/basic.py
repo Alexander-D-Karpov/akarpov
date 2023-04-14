e56f64afc6aa4cf2b34bd1d9f484a14ec06c05c1
Alexandr Karpov <alexandr.d.karpov@gmail.com>
2023-04-14 10:23:48 +0300

    fixed adaptive

--- akarpov/files/previews/video/basic.py
+++ akarpov/files/previews/video/basic.py
@@ -11,7 +11,7 @@ def view(file: File) -> (str, str):
     <meta property="og:video:height" content="480" />
     <link href="https://vjs.zencdn.net/8.0.4/video-js.css" rel="stylesheet" />
     """
-    data = """"playbackRates": [0.5, 1, 1.5, 2], "responsive": true }"""
+    data = """{"playbackRates": [0.5, 1, 1.5, 2], "responsive": true, "fit": true, "fluid": true }"""
     content = f"""
 
       <video id="my_video_1" class="video-js vjs-default-skin" height="500px"
