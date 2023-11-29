1644fa54034b704c5ddc06f0a654a99a7ccd059c
Alexander-D-Karpov <alexandr.d.karpov@gmail.com>
2023-11-29 17:36:33 +0300

    updated local compose

--- config/asgi.py
+++ config/asgi.py
@@ -4,8 +4,14 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf import settings
 from django.core.asgi import get_asgi_application
 
-from akarpov.common.channels import HeaderAuthMiddleware
-from config import routing
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+
+from akarpov.common.channels import HeaderAuthMiddleware  # noqa
+from config import routing  # noqa
 
 if settings.DEBUG:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
@@ -14,7 +20,7 @@ else:
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": HeaderAuthMiddleware(URLRouter(routing.websocket_urlpatterns)),
     }
 )
