dda9df23a04e64c5e1773f606263ec3304a9f04d
sanspie <sanspie@akarpov.ru>
2026-03-23 21:39:43 +0300

    updates

--- akarpov/music/consumers.py
+++ akarpov/music/consumers.py
@@ -9,13 +9,44 @@ from akarpov.music.models import RadioSong
 from akarpov.users.models import User
 
 
-class RadioConsumer(BaseConsumer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(args, kwargs)
-        self.room_group_name = None
+class DownloadProgressConsumer(BaseConsumer):
+    group_name = "download_progress"
 
     async def connect(self):
-        self.room_group_name = "radio_main"
+        user_id = self.scope.get("user_id")
+        if not user_id:
+            await self.close()
+            return
+
+        is_super = await self._is_superuser(user_id)
+        if not is_super:
+            await self.close()
+            return
+
+        await self.accept()
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+
+    @database_sync_to_async
+    def _is_superuser(self, user_id):
+        try:
+            return User.objects.get(id=user_id).is_superuser
+        except User.DoesNotExist:
+            return False
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def receive(self, text_data=None, bytes_data=None, **kwargs):
+        pass
+
+    async def download_update(self, event):
+        await self.send_json(event["data"])
+
+
+class RadioConsumer(BaseConsumer):
+    room_group_name = "radio_main"
+
+    async def connect(self):
         await self.accept()
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         data = await self.get_radio_song()
@@ -48,39 +79,3 @@ class RadioConsumer(BaseConsumer):
     async def song(self, event):
         data = event["data"]
         await self.send_json(data)
-
-
-class DownloadProgressConsumer(BaseConsumer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(args, kwargs)
-        self.group_name = "download_progress"
-
-    async def connect(self):
-        user_id = self.scope.get("user_id")
-        if not user_id:
-            await self.close()
-            return
-
-        is_super = await self._is_superuser(user_id)
-        if not is_super:
-            await self.close()
-            return
-
-        await self.accept()
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
-
-    @database_sync_to_async
-    def _is_superuser(self, user_id):
-        try:
-            return User.objects.get(id=user_id).is_superuser
-        except User.DoesNotExist:
-            return False
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
-
-    async def receive(self, text_data=None, bytes_data=None, **kwargs):
-        pass
-
-    async def download_update(self, event):
-        await self.send_json(event["data"])
